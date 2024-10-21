16c8f328f7b375e3bfd9eba41a87befc9ddb526b
Armando <agarcia@appdev.yearup.org>
2024-10-21 15:40:13 -0500

    Lab 1 excercises

---
+++ hello.py
@@ -0,0 +1,7 @@
+message = 'Hello world!'
+print(message)
+
+dollars = 3
+cents = .5
+
+print(dollars + cents)
---
+++ variable_examples.py
@@ -0,0 +1,45 @@
+# Customer ID
+customer_id = 12345
+
+# Customer's first, middle, and last name
+first_name = "John"
+middle_name = "Michael"
+last_name = "Doe"
+
+# Gender
+gender = "Male"
+
+# Date of birth
+date_of_birth = "1990-05-12"
+
+# Driver's license number
+drivers_license_number = "D123456789"
+
+# Auto policy number
+auto_policy_number = "AP123456789"
+
+
+# What is the full list of reserved words that can’t be used for variable names?
+''' False	def	if	raise
+None	del	import	return
+True	elif	in	try
+and	else	is	while
+as	except	lambda	with
+assert	finally	nonlocal	yield
+break	for	not
+class	form	or
+continue	global	pass'''
+
+''' Pick 5 of these words and review the explanation for how it is used as a keyword in
+Python. Add these 5 definitions as comments to your exercise document. Put ^^
+around any terms that you are not familiar with '''
+
+'''A lambda function in Python is an ^^anonymous function^^. It can take any number of arguments but only have a single expression.
+
+The yield function ends a function and returns an ^^iterator.^^
+
+Accessing a global variable is simple as any other variable but to modify a global variable, you need to use the ^^global keyword.^^
+
+The finally statement is an optional part of ^^try… except error^^. It always executes the code regardless of whether an error was thrown or not.
+
+The try statement is used to make a ^^try… except statement^^. The try statement starts a block of code that is tried to execute. If it fails, the except block catches the error.'''
